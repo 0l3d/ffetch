39df2ab42ec21fbe3731f5144a8d484605961463
cd <137810872+0l3d@users.noreply.github.com>
2023-07-10 22:57:12 +0300

    Add files via upload

---
+++ src/config.rs
@@ -0,0 +1,28 @@
+mod core;
+
+use crate::config::core::ffetch;
+
+pub fn get_ascii() -> String {
+    return format!("        
+        ----------------------------------------------------
+        |                F-FETCH
+        | USERNAME : {}@{}
+        | PLATFORM : {}
+        | OS NAME : {}
+        | KERNEL VERSION : {}
+        | DESKTOP ENV : {}
+        | MEMORY : {} MB
+        | CPU : {} | {}
+        |                                        
+        ---------------------------------------------------- ",
+        ffetch::get_username(),
+        ffetch::get_hostname(),
+        ffetch::get_platform(),
+        ffetch::get_os_name(),
+        ffetch::get_kernel_version(),
+        ffetch::get_desktop_env(),
+        ffetch::get_memory(),
+        ffetch::get_cpu_name(),
+        ffetch::get_cpu_arch()
+    );
+}
