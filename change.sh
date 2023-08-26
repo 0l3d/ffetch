b992b20ba8df7ffd4a54a904fd5dd28becf624f4
cd <137810872+0l3d@users.noreply.github.com>
2023-08-26 14:04:00 +0300

    Delete change.sh

--- change.sh
+++
@@ -1,5 +0,0 @@
-cat << EOF > ~/.config/ffetch/config.yml
-ascii_path : "/home/$(whoami)/.config/ffetch/ascii_arts/debian.txt"
-#all components : user.host,platform,os.name,memory,cpu,uptime,user.name,host.name,kernel.version,de,packages,gpu,shell
-components : "user.host,platform,os.name,shell,memory,cpu,gpu,uptime,packages"
-EOF
