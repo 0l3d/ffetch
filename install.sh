e6859aadff7564e28b981048a99cc72514394ec6
cd <137810872+0l3d@users.noreply.github.com>
2023-08-26 14:11:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,20 +2,13 @@ git clone https://github.com/0l3d/ffetch.git
 mkdir ~/.config/ffetch
 cd ffetch
 cat << EOF > ~/.config/ffetch/config.yml
-<<<<<<< HEAD
-ascii_path : "/home/$(whoami)/.config/ffetch/ascii_arts/linux.txt"
-=======
-ascii_path : "/home/$(whoami)/.config/ffetch/ascii_arts/afplusplus.txt"
->>>>>>> 4bef7bc (Update installer script)
+ascii_path : "/home/user/.config/ffetch/ascii_arts/fedora.txt"
 #all components : user.host,platform,os.name,memory,cpu,uptime,user.name,host.name,kernel.version,de,packages,gpu,shell
 components : "user.host,platform,os.name,shell,memory,cpu,gpu,uptime,packages"
+ascii_color : "c.blue;"
+colors : "c.red,c.green,c.yellow,c.blue,c.magenta,c.white,c.white,c.yellow,c.green"
 EOF
 mv ascii_arts ~/.config/ffetch
-sudo mv ./release/ffetch /usr/bin/
+cargo install
 cd ..
 rm -rf ./ffetch
-<<<<<<< HEAD
-
-echo "Installed!"
-=======
->>>>>>> 4bef7bc (Update installer script)
