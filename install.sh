05e177cea88c9bfe7169ca6333c9bda576581a13
cd <137810872+0l3d@users.noreply.github.com>
2023-08-27 21:24:05 +0300

    fixed read ascii error

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ git clone https://github.com/0l3d/ffetch.git
 mkdir ~/.config/ffetch
 cd ffetch
 cat << EOF > ~/.config/ffetch/config.yml
-ascii_path : "/home/user/.config/ffetch/ascii_arts/fedora.txt"
+ascii_path : "/home/$(whoami)/.config/ffetch/ascii_arts/fedora.txt"
 #all components : user.host,platform,os.name,memory,cpu,uptime,user.name,host.name,kernel.version,de,packages,gpu,shell
 components : "user.host,platform,os.name,shell,memory,cpu,gpu,uptime,packages"
 ascii_color : "c.blue;"
